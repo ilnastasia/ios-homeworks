f5580d36ec8b1ff1b204c944a88836072e988796
ilnastasia <nastya7552@mail.ru>
2021-11-27 00:18:42 +0300

    new files added

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Anastasia Ilina on 26.11.2021.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var labelBirthDate: UILabel!
+    @IBOutlet weak var labelCity: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var text: UITextView!
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    
+
+}
