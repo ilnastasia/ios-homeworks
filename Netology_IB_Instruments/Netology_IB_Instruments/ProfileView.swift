a507fe2f5e14623c6bf06af240f8ec8642011824
ilnastasia <nastya7552@mail.ru>
2021-12-12 18:27:08 +0300

    удалены файлы с тестами, исправлены недочеты

--- Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -14,15 +14,6 @@ class ProfileView: UIView {
     @IBOutlet weak var labelCity: UILabel!
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var text: UITextView!
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
     
-    
-
+   
 }
--- Netology_IB_Instruments/Netology_IB_Instruments/ProfileViewController.swift
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileViewController.swift
@@ -1,29 +1,13 @@
-//
-//  ProfileViewController.swift
-//  Netology_IB_Instruments
-//
-//  Created by Anastasia Ilina on 26.11.2021.
-//
 
 import UIKit
 
 class ProfileViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()  {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let profileView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as! UIView
+        self.view = profileView
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+        
 }
