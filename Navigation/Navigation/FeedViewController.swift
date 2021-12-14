f87ecf6956210f206d074bbb5ed1155cf3eb73e5
ilnastasia <nastya7552@mail.ru>
2021-12-14 23:05:20 +0300

    добавлена верхняя панель

--- Navigation/Navigation/FeedViewController.swift
+++ Navigation/Navigation/FeedViewController.swift
@@ -43,6 +43,7 @@ class FeedViewController: UIViewController {
     }
     
     func UpperBorder() {
+        
         let upperBorderView = UIView()
          
         upperBorderView.backgroundColor = .white
@@ -51,6 +52,8 @@ class FeedViewController: UIViewController {
         let upperBorderWidth = UIScreen.main.bounds.width
         let upperBorderHeight = UIScreen.main.bounds.height
         
+        upperBorderView.translatesAutoresizingMaskIntoConstraints = false
+        
         let widthConstraint = NSLayoutConstraint(item: upperBorderView,
             attribute: .width,
             relatedBy: .equal,
@@ -58,16 +61,30 @@ class FeedViewController: UIViewController {
             attribute: .notAnAttribute,
             multiplier: 1.0, constant: upperBorderWidth)
 
-        
         let heightConstraint = NSLayoutConstraint(item: upperBorderView,
             attribute: .height,
             relatedBy: .equal,
             toItem: nil,
             attribute: .notAnAttribute,
-            multiplier: 1.0, constant: upperBorderHeight/7)
+            multiplier: 1.0, constant: upperBorderHeight/9)
         
-        view.addConstraint(widthConstraint)
-        view.addConstraint(heightConstraint)
+        let horizontalConstraint = NSLayoutConstraint(item: upperBorderView,
+            attribute: .centerX,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .centerX,
+            multiplier: 1.0,
+            constant: 0)
+        
+        let verticalConstraint = NSLayoutConstraint(item: upperBorderView,
+            attribute: .top,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .top,
+            multiplier: 1.0,
+            constant: 0)
+            
+        NSLayoutConstraint.activate([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
     }
 }
 
--- Navigation/Navigation/Profile/ProfileViewController.swift
+++ Navigation/Navigation/Profile/ProfileViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ProfileViewController.swift
-//  Navigation
-//
-//  Created by Anastasia Ilina on 14.12.2021.
-//
 
 import Foundation
 import UIKit
@@ -11,6 +5,7 @@ import UIKit
 class ProfileViewController : UIViewController {
     
     var profileHeadView : ProfileHeaderView?
+    var feedController: FeedViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,13 +15,56 @@ class ProfileViewController : UIViewController {
         
         profileHeadView = ProfileHeaderView()
         view.addSubview(profileHeadView!)
+       
+        UpperBorder()
     }
     
     override func viewWillLayoutSubviews() {
         profileHeadView!.frame = view.frame
     }
     
-    
-    
-    
+    func UpperBorder() {
+        
+        let upperBorderView = UIView()
+         
+        upperBorderView.backgroundColor = .white
+        view.addSubview(upperBorderView)
+        
+        let upperBorderWidth = UIScreen.main.bounds.width
+        let upperBorderHeight = UIScreen.main.bounds.height
+        
+        upperBorderView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let widthConstraint = NSLayoutConstraint(item: upperBorderView,
+            attribute: .width,
+            relatedBy: .equal,
+            toItem: nil,
+            attribute: .notAnAttribute,
+            multiplier: 1.0, constant: upperBorderWidth)
+
+        let heightConstraint = NSLayoutConstraint(item: upperBorderView,
+            attribute: .height,
+            relatedBy: .equal,
+            toItem: nil,
+            attribute: .notAnAttribute,
+            multiplier: 1.0, constant: upperBorderHeight/9)
+        
+        let horizontalConstraint = NSLayoutConstraint(item: upperBorderView,
+            attribute: .centerX,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .centerX,
+            multiplier: 1.0,
+            constant: 0)
+        
+        let verticalConstraint = NSLayoutConstraint(item: upperBorderView,
+            attribute: .top,
+            relatedBy: .equal,
+            toItem: view,
+            attribute: .top,
+            multiplier: 1.0,
+            constant: 0)
+            
+        NSLayoutConstraint.activate([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
+    }
 }
