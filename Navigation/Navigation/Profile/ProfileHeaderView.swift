ee86054d695b3f47ec1eb81a085d8aa5019f14c9
ilnastasia <nastya7552@mail.ru>
2021-12-17 02:36:59 +0300

    добавлено поле ввода текста в профиль, обработка кнопки доработана

--- Navigation/Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Navigation/Profile/ProfileHeaderView.swift
@@ -19,25 +19,40 @@ class ProfileHeaderView : UIView {
     let nameView = UILabel()
     let descriptionView = UILabel()
     let statusButton = UIButton(type: .system)
+    let statusField = UITextField()
+    
+    private var statusText: String {
+        get { return descriptionView.text! }
+        set { descriptionView.text = newValue }
+    }
+    
+    let saveName = UserDefaults.standard
     
     override func draw(_ rect: CGRect) {
         
         profileController = ProfileViewController()
         
+        let thisHeight = profileController?.screenHeight
+        let thisWidth = profileController?.screenWidth
+        
         avatarView.frame = CGRect(
             x: 16,
-            y: profileController!.screenHeight/9 + 16,
-            width: profileController!.screenWidth/3.5,
-            height: profileController!.screenWidth/3.5)
+            y: thisHeight!/9 + 16,
+            width: thisWidth!/3.5,
+            height: thisWidth!/3.5)
         avatarView.layer.borderWidth = 3.0
         avatarView.layer.borderColor = UIColor.white.cgColor
-        avatarView.layer.cornerRadius = profileController!.screenWidth/7
+        avatarView.layer.cornerRadius = thisWidth!/7
         avatarView.layer.contents = UIImage(named: "hedgehog")?.cgImage
         avatarView.clipsToBounds = true
         addSubview(avatarView)
         
         
-        nameView.frame = CGRect(x: profileController!.screenWidth/2.75, y: profileController!.screenHeight/9 + 27, width: 250, height: 21)
+        nameView.frame = CGRect(
+            x: thisWidth!/2.75,
+            y: thisHeight!/9 + 27,
+            width: 250,
+            height: 21)
         nameView.textColor = .black
         nameView.textAlignment = .justified
         nameView.font = UIFont(name: "Helvetica-Bold", size : 18)
@@ -45,7 +60,11 @@ class ProfileHeaderView : UIView {
         addSubview(nameView)
         
         
-        descriptionView.frame = CGRect(x: profileController!.screenWidth/2.75, y: profileController!.screenWidth/3.5 + profileController!.screenHeight/9 - 20, width: 250, height: 18)
+        descriptionView.frame = CGRect(
+            x: thisWidth!/2.75,
+            y: thisWidth!/3.5 + thisHeight!/9 - 20,
+            width: 250,
+            height: 18)
         descriptionView.textColor = .gray
         descriptionView.textAlignment = .justified
         descriptionView.font = UIFont(name: "Helvetica-Regular", size : 14)
@@ -53,7 +72,11 @@ class ProfileHeaderView : UIView {
         addSubview(descriptionView)
         
         
-        statusButton.frame = CGRect(x: 16, y: 32 + profileController!.screenWidth/3.5 + profileController!.screenHeight/9, width: profileController!.screenWidth - 32, height: 50)
+        statusButton.frame = CGRect(
+            x: 16,
+            y: 58 + thisWidth!/3.5 + thisHeight!/9,
+            width: thisWidth! - 32,
+            height: 50)
         statusButton.setTitle("Установить статус", for: .normal)
         statusButton.backgroundColor = .systemBlue
         statusButton.setTitleColor(.white, for: .normal)
@@ -64,9 +87,33 @@ class ProfileHeaderView : UIView {
         statusButton.layer.shadowOpacity = 0.7
         statusButton.addTarget(self, action:#selector(self.statusButtonClicked), for: .touchUpInside)
         addSubview(statusButton)
+        
+        statusField.frame = CGRect(
+            x: thisWidth!/2.75,
+            y: thisWidth!/3.5 + thisHeight!/9 + 3,
+            width: (thisWidth! - 16) - thisWidth!/2.75,
+            height: 40)
+        statusField.backgroundColor = .white
+        statusField.layer.borderColor = UIColor.black.cgColor
+        statusField.layer.borderWidth = 1
+        statusField.layer.cornerRadius = 12
+        statusField.textColor = .black
+        statusField.font = UIFont(name: "Helvetica-Regular", size: 15)
+        statusField.addTarget(self, action:#selector(statusTextChanged(_:)), for: .editingChanged)
+        addSubview(statusField)
+        
+        guard let text = saveName.object(forKey: "descriptionView") as? String else { return }
+            descriptionView.text = text
+    }
+    
+    @objc func statusTextChanged(_ textField: UITextField) {
+        statusText = textField.text!
     }
 
     @objc func statusButtonClicked() {
+        saveName.set(descriptionView.text, forKey: "descriptionView")
         print(descriptionView.text!)
     }
+    
+    
 }
